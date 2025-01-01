ceee74f81eea985281ed3b080eb62ce5e3f8b7d9
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-01-02 00:55:56 +0530

    Some change

--- Python Adventures/pythonProject/Functions/LambdaFunc.py
+++ Python Adventures/pythonProject/Functions/LambdaFunc.py
@@ -1,7 +1,14 @@
 # Temporary Function But Only Expression
-func = lambda b : b * 5
-print(func(5)) # 25
+# func = lambda b : b * 5
+# print(func(5)) # 25
+#
+#
+# func2 = lambda a,b : a * b
+# print(func2(5,6)) # 30
 
+add = lambda *ele: sum(ele)
+res = add(1,2,3,4)
+print(res)
 
-func2 = lambda a,b : a * b
-print(func2(5,6)) # 30
+res = list(map(lambda x: x*2, [1,2,3,4]))
+print(res)
